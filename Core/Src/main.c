48fe5ec120ef38132dce6acf2df8a46d6b192a0b
freehelot <hrvoje.zmavc@hotmail.com>
2021-06-25 19:36:55 +0200

    Cleanup of unused files

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,15 +19,14 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "spi.h"
-#include "tim.h"
-#include "usart.h"
-#include "gpio.h"
+//#include "spi.h"
+//#include "tim.h"
+//#include "usart.h"
+//#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <bits.h>
-#include <mw_tmc2130_io.h>
 #include <register.h>
 #include <stdbool.h>
 
@@ -52,6 +51,7 @@
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
 #define DELAY_MS                  (10U)
+#define MODE_SELECT               (5U)
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -97,6 +97,7 @@ int main(void)
 	uint32_t movement = 0;
 	bool valid = true;
 	bool direction = true;
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -106,11 +107,14 @@ int main(void)
   /* Initialize all configured peripherals */
   bsp_init();
 
+  mw_tmc2130_io_init();
+  mw_tmc2130_io_config_all(MODE_SELECT);
   /* USER CODE BEGIN 2 */
-  //HAL_Delay(2000);
-  tmc_io_gpio();
-  tmc_io_init();//set all regs to zero
-  tmc_io_config(5);
+
+
+  //tmc_io_gpio();
+  //tmc_io_init();//set all regs to zero
+  //tmc_io_config(5);
 
   //HAL_Delay(2000);
   //tmc_get_status();
@@ -118,93 +122,94 @@ int main(void)
   //timer2 init
   //timer2_Ticks_usec=0;
   //init gpio for tmc, DIR, STEP, EN to high (EN = 1, disabled, EN = 0, enabled)
-  HAL_GPIO_WritePin(EN_GPIO_Port, EN_Pin, GPIO_PIN_RESET);
-  int32_t pulses = 0;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-    /* USER CODE BEGIN 3 */
-	  timer2_wait_usec(5);
-	  // Expect XXX numbers
-	  // Checks if the command in mm has been given
-	  if(!check)
-	  {
-		  // Waits for command via uart
-		  if(USART2_Dequeue(&c)!=0)
-		  	  {
-			  	  	  // Check first symbol
 
-		  			  if(valid)
-		  			  {
-		  				 USART2_SendChar(c);
-		  				 if(c == '-')
-		  				 {
-		  					 direction = false;
-		  				 }
-		  				 else
-		  				 {
-		  					 direction = true;
-		  				 }
-		  				 valid = false;
-		  			  }
-		  			  else
-		  			  {
-		  				  USART2_SendChar(c);
-		  				  command[counter]=c;
-		  				  counter++;
-		  				  if(counter > (size - 1))
-		  				  {
-		  					  counter = 0; // Resets counter
-		  					  check = true; // Command received
-		  					  valid = true;
-		  					  movement = (uint32_t)atoi(command);
-		  					  /*if(movement > 255)
-								{
-									movement = 240;
-		  						}
-		  						else if(movement < 0)
-		  						{
-		  						  	movement = 0;
-		  						}*/
-		  				  }
-
-		  			  }
-
-		  	  }
-	  }
-	  if(check)
-	  {
-		  //pulses = (FULL_ANGLE * movement * PITCH_DIV * STEP_ANGLE_DIV) / (STEP_ANGLE * PITCH_DIAM);
-		  //pulses = ( movement  * STEP_ANGLE_DIV) / (STEP_ANGLE);
-		  //pulses = (2*2*movement * 641)/ 314 ;
-		  pulses = (2*2*movement * 65)/ 30 ;
-		  for(uint32_t i = 0; i<pulses; i++)
-		  {
-			  tmc_step(20,direction);
-		  }
-		  check = false; // command fullfilled
-	  }
-
-
-	  //tmc_step(100);
-			//HAL_SPI_Transmit(&hspi1, drv, 5, 20);
-			//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_SET);
-
-		  	//HAL_Delay(100);
-		  	//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_RESET);
-		  	//tmc_io_write(REG_CHOPCONF,set_chopconf(0,0 , 0, 0, 0, 0, 0));
-			//HAL_SPI_Transmit(&hspi1, drv, 5, 20);
-			//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_SET);
-
-		  	//HAL_Delay(100);
-		  	//tmc_io_write(TMC2130_WRITE, set_chopconf_struct(choppy));
-
-
-
+//    /* USER CODE END WHILE */
+//    /* USER CODE BEGIN 3 */
+//	  //timer2_wait_usec(5);
+//	  // Expect XXX numbers
+//	  // Checks if the command in mm has been given
+//	  if(!check)
+//	  {
+//		  // Waits for command via uart
+//		  if(USART2_Dequeue(&c)!=0)
+//		  	  {
+//			  	  	  // Check first symbol
+//
+//		  			  if(valid)
+//		  			  {
+//		  				 USART2_SendChar(c);
+//		  				 if(c == '-')
+//		  				 {
+//		  					 direction = false;
+//		  				 }
+//		  				 else
+//		  				 {
+//		  					 direction = true;
+//		  				 }
+//		  				 valid = false;
+//		  			  }
+//		  			  else
+//		  			  {
+//		  				  USART2_SendChar(c);
+//		  				  command[counter]=c;
+//		  				  counter++;
+//		  				  if(counter > (size - 1))
+//		  				  {
+//		  					  counter = 0; // Resets counter
+//		  					  check = true; // Command received
+//		  					  valid = true;
+//		  					  movement = (uint32_t)atoi(command);
+//		  					  /*if(movement > 255)
+//								{
+//									movement = 240;
+//		  						}
+//		  						else if(movement < 0)
+//		  						{
+//		  						  	movement = 0;
+//		  						}*/
+//		  				  }
+//
+//		  			  }
+//
+//		  	  }
+//	  }
+//	  if(check)
+//	  {
+//		  //pulses = (FULL_ANGLE * movement * PITCH_DIV * STEP_ANGLE_DIV) / (STEP_ANGLE * PITCH_DIAM);
+//		  //pulses = ( movement  * STEP_ANGLE_DIV) / (STEP_ANGLE);
+//		  //pulses = (2*2*movement * 641)/ 314 ;
+//		  pulses = (2*2*movement * 65)/ 30 ;
+//		  for(uint32_t i = 0; i<pulses; i++)
+//		  {
+//			  tmc_step(20,direction);
+//		  }
+//		  check = false; // command fullfilled
+//	  }
+//
+//
+//	  //tmc_step(100);
+//			//HAL_SPI_Transmit(&hspi1, drv, 5, 20);
+//			//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_SET);
+//
+//		  	//HAL_Delay(100);
+//		  	//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_RESET);
+//		  	//tmc_io_write(REG_CHOPCONF,set_chopconf(0,0 , 0, 0, 0, 0, 0));
+//			//HAL_SPI_Transmit(&hspi1, drv, 5, 20);
+//			//HAL_GPIO_WritePin(CS_1_GPIO_Port,CS_1_Pin,GPIO_PIN_SET);
+//
+//		  	//HAL_Delay(100);
+//		  	//tmc_io_write(TMC2130_WRITE, set_chopconf_struct(choppy));
+//
+//
+//
+//  }
   }
   /* USER CODE END 3 */
 }
